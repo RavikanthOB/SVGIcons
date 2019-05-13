8ece88f4c5ebfef26646ffcb3917997068b0158f
Ravikanth OB <ravikanth.xampr@outlook.com>
2019-05-13 20:26:33 +0530

    svgIcons basic setup

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'svgicons'
+import Icon from 'svgicons'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{display:'flex',justifyContent:'center',alignContent:'center',alignItems:'center',height:'100vh'}}>
+        <Icon name='phone' size={26} color={'green'}  />
+        <Icon name='wifi' size={40} color={'green'}  />
+        <Icon name='home' size={16} color={'red'}  />
+        <Icon name='send' size={16} color={'red'}  />
+        <Icon name='addplus' size={46} color={'blue'}  />
+        <Icon name='dots-vertical' size={46} color={'blue'}  />
+        <Icon name='clock' size={46} color={'blue'}  />
+        <Icon name='magnify' size={46} color={'blue'}  />
+        <Icon name='palette' size={46} color={'blue'}  />
+        <Icon name='delete' size={46} color={'#b7b8b8'}  />
+        <Icon name='group' size={46} color={'red'}  />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import Wifi from './Icons/Wifi'
+import Phone from './Icons/Phone'
+import Trash from './Icons/Trash'
+import Message from './Icons/Message'
+import Envelope from './Icons/Envelope'
+import World from './Icons/World'
+import Mobile from './Icons/Mobile'
+import AddPlus from './Icons/AddPlus'
+import Clock from './Icons/Clock'
+import LeftArrow from './Icons/LeftArrow'
+import Home from './Icons/Home'
+import DotsVertical from './Icons/DotsVertical'
+import Search from './Icons/Search'
+import Palette from './Icons/Palette'
+import Send from './Icons/Send'
+import Group from './Icons/Group'
+// var context = require.context('./Icons', false, /\.js$/)
 
-import styles from './styles.css'
+const listOFIcons = {
+  'phone': (props) => <Phone {...props} />,
+  'wifi': (props) => <Wifi {...props} />,
+  'addplus': (props) => <AddPlus {...props} />,
+  'arrow-left': (props) => <LeftArrow {...props} />,
+  'clock': (props) => <Clock {...props} />,
+  'home': (props) => <Home {...props} />,
+  'dots-vertical': (props) => <DotsVertical {...props} />,
+  'magnify': (props) => <Search {...props} />,
+  'palette': (props) => <Palette {...props} />,
+  'send': (props) => <Send {...props} />,
+  'delete': (props) => <Trash {...props} />,
+  'group': (props) => <Group {...props} />
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
 }
+
+const Icon = props => {
+  return listOFIcons[props.name](props)
+}
+
+export default Icon
